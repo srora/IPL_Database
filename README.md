cbec2965ccff67ceabe094c392ccf2adb44ee2fe
Sarisht Wadhwa <sarishtwadhwa@Kumuds-MacBook-Air.local>
2018-02-17 21:36:42 +0530

    q4,5

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IPL_Database
 COL 362 Assignment 2 IPL Database SQL
+Test all Queries in the end
--- main.sql
+++ main.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Player(
+/*CREATE TABLE Player(
 	player_id integer,
 	player_name text,
 	dob date,
@@ -83,39 +83,45 @@ CREATE TABLE Extra_runs(
 );
 
 COPY Player(player_id, player_name, dob, batting_hand, bowling_skill, country_name) 
-FROM 'F:\sem8\col362\Assignments\assign2\player.csv' DELIMITER ',';
+FROM '/Users/sarishtwadhwa/Downloads/DATA/player.csv' DELIMITER ',';
 
 COPY Team(team_id, name)
-FROM 'F:\sem8\col362\Assignments\assign2\team.csv' DELIMITER ',';
+FROM '/Users/sarishtwadhwa/Downloads/DATA/team.csv' DELIMITER ',';
 
 COPY Match(match_id, team_1, team_2, match_date, season_id, venue, toss_winner, toss_decision, win_type, win_margin, outcome_type, match_winner, man_of_the_match) 
-FROM 'F:\sem8\col362\Assignments\assign2\match.csv' DELIMITER ',';
+FROM '/Users/sarishtwadhwa/Downloads/DATA/match.csv' DELIMITER ',';
 
 COPY Player_match(match_id, player_id, role, team_id)
-FROM 'F:\sem8\col362\Assignments\assign2\player_match.csv' DELIMITER ',';
+FROM '/Users/sarishtwadhwa/Downloads/DATA/player_match.csv' DELIMITER ',';
 
 COPY Ball_by_ball(match_id, over_id, ball_id, innings_no, team_batting, team_bowling, striker_batting_position, striker, non_striker, bowler)
-FROM 'F:\sem8\col362\Assignments\assign2\ball_by_ball.csv' DELIMITER ',';
+FROM '/Users/sarishtwadhwa/Downloads/DATA/ball_by_ball.csv' DELIMITER ',';
 
 COPY Batsman_scored(match_id, over_id, ball_id, runs_scored, innings_no)
-FROM 'F:\sem8\col362\Assignments\assign2\batsman_scored.csv' DELIMITER ',';
+FROM '/Users/sarishtwadhwa/Downloads/DATA/batsman_scored.csv' DELIMITER ',';
 
 COPY Wicket_taken(match_id, over_id, ball_id, player_out, kind_out, innings_no)
-FROM 'F:\sem8\col362\Assignments\assign2\wicket_taken.csv' DELIMITER ',';
+FROM '/Users/sarishtwadhwa/Downloads/DATA/wicket_taken.csv' DELIMITER ',';
 
 COPY Extra_runs(match_id, over_id, ball_id, extra_type, extra_runs, innings_no)
-FROM 'F:\sem8\col362\Assignments\assign2\extra_runs.csv' DELIMITER ',';
-
+FROM '/Users/sarishtwadhwa/Downloads/DATA/extra_runs.csv' DELIMITER ',';
+*/
 --1--
 
 SELECT player_name FROM Player WHERE batting_hand LIKE 'Left%' AND country_name='England' ORDER BY player_name;
 
 --2--
---doubt : is age rounded off or not 28 years 7 months is 28 years or 29??? our query considers 27.57 as 27
+--doubt : is age rounded off or not 28 years 7 months is 28 years or 29??? our query considers 27.57 as 27 -- Reply This seems fine for both cases, since 28 yrs 7 months is still smaller than 29.
 SELECT player_name, age FROM (SELECT player_name, dob, EXTRACT(YEAR FROM(AGE('2018-02-12', dob))) AS age FROM Player WHERE bowling_skill='Legbreak googly') AS dummy WHERE age>=28 ORDER BY age DESC, player_name ASC;
 
 --3--
-
-SELECT match_id, match_winner FROM Match where toss_decision='bat' ORDER BY match_id;
+SELECT match_id, toss_winner FROM Match where toss_decision='bat' ORDER BY match_id;
 
 --4--
+SELECT over_id,SUM(runs_scored) AS over_runs_scored FROM Batsman_scored WHERE match_id = 335987 GROUP BY over_id HAVING SUM(runs_scored)<=7 ORDER BY over_runs_scored DESC,over_id
+
+--5--
+SELECT DISTINCT player_name FROM wicket_taken,player WHERE kind_out = 'bowled' AND player_out = player_id ORDER BY player_name
+
+--6--
+
